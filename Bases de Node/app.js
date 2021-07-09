1ec40f911b7fa3151becb14607d0d7a1d489a194
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-09 09:58:31 -0500

    Regresando una promesa desde un modulo

--- Bases de Node/app.js
+++ Bases de Node/app.js
@@ -4,3 +4,5 @@ const {crearArchivoTabla} = require("./helpers/multiplicar");
 
 const base = 6;
 crearArchivoTabla(base)
+    .then(nombreArchivo=>console.log(`${nombreArchivo} creado!`))
+    .catch(err=>console.log(err))
--- Bases de Node/helpers/multiplicar.js
+++ Bases de Node/helpers/multiplicar.js
@@ -2,21 +2,25 @@ const multiplicar = (base=1,n)=>base*n;
 
 const fs = require("fs");
 
-const crearArchivoTabla = (base) =>{
-    let salida = "";
-    const path = `./Tabla_del_${base}.txt`
-
-    salida+="============\n"
-    salida+=`Tabla del ${base}\n`
-    salida+="============\n"
-
-
-    for (let i = 1; i < 11; i++) {
-        salida += (`${base} x ${i} = ${multiplicar(base,i)}\n`);
+const crearArchivoTabla = async(base=1) =>{
+    try {
+        let salida = "";
+        const nombreArchivo = `./Tabla_del_${base}.txt`
+    
+        salida+="============\n"
+        salida+=`Tabla del ${base}\n`
+        salida+="============\n"
+    
+    
+        for (let i = 1; i < 11; i++) {
+            salida += (`${base} x ${i} = ${multiplicar(base,i)}\n`);
+        }
+    
+        fs.writeFileSync(nombreArchivo,salida)
+        return `${nombreArchivo}`
+    } catch (error) {
+        throw "No se pudo completar la escritura    ";
     }
-
-    fs.writeFileSync(path,salida)
-    console.log(`${path} creada`)
 }
 
 module.exports = {crearArchivoTabla}
