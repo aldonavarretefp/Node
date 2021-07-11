dc57b2e00b14303f30e15b30d4ea18fcf3554180
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-11 15:38:52 -0500

    Yargs en un archivo aparte

--- Bases de Node/app.js
+++ Bases de Node/app.js
@@ -3,26 +3,8 @@ const { type } = require("os");
 var colors = require('colors');
 const { alias, demandOption } = require("yargs");
 const {crearArchivoTabla} = require("./helpers/multiplicar");
-const argv = require("yargs")
-                .option('b',{
-                    alias: 'base',
-                    type: 'number',
-                    demandOption:true
-                })
-                .option('l',{
-                    alias:'list',
-                    type:'boolean',
-                    default: false
-                })
-                .check((argv,options)=>{
-                    if (isNaN(argv.base)) throw 'ERROR: La base debe ser un numero'.red; 
-                    return true;
-                })
-                .argv;
-
-// console.log(process.argv)
-// console.log(argv)
+const argv = require("./config/yargs");
 
 crearArchivoTabla(argv.base,argv.l)
-                .then(nombreArchivo=>console.log(`${nombreArchivo} ha sido creado exitosamente!"`.green))
+                .then(nombreArchivo=>console.log(`${nombreArchivo.rainbow} ha sido creado ${colors.green("exitosamente")}!`))
                 .catch(err=>console.log(err.red))
---
+++ Bases de Node/config/yargs.js
@@ -0,0 +1,20 @@
+const argv = require("yargs")
+                .option('b',{
+                    alias: 'base',
+                    type: 'number',
+                    demandOption:true,
+                    describe: "Es la base de la tabla de multiplicar"
+                })
+                .option('l',{
+                    alias:'listar',
+                    type:'boolean',
+                    default: false,
+                    describe: "Muestra la tabla en consola"
+                })
+                .check((argv,options)=>{
+                    if (isNaN(argv.base)) throw 'ERROR: La base debe ser un numero'.red; 
+                    return true;
+                })
+                .argv;
+
+module.exports = argv;
