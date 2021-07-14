86b901f9f634c355babc84464836dc88638a6be7
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-14 13:40:00 -0500

    Haciendo peticion desde consola a la API de Mapbox con nuestro TOKEN

--- 05_Clima_app/models/busquedas.ts
+++ 05_Clima_app/models/busquedas.ts
@@ -9,7 +9,7 @@ class Busquedas {
     async buscarCiudad( lugar:string ):Promise<string[]>{
         //Peticion http
         try {
-            const resp = await axios.get("https://reqres.in/api/users?page=2");
+            const resp = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/M%C3%A9xico.json?access_token=pk.eyJ1IjoiYWxkb25hdmFycmV0ZSIsImEiOiJja3Izc3pxamkxNGVtMnV0Y3Qxc2JicGFrIn0.6efzyciGBBR9rAxHwnao8g&limit=5&language=es");
             console.log(resp);
             return []; //Retornar los lugares que coincidan    
         } catch (error) {
