c05420d6350a80bdda958275544d7d1906adb38a
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-14 13:56:24 -0500

    Creando la instancia de configuracion de axios, definiendo parametros y haciendo peticion get de la ciudad que queramos

--- 05_Clima_app/models/busquedas.ts
+++ 05_Clima_app/models/busquedas.ts
@@ -5,12 +5,27 @@ class Busquedas {
     constructor(){
         //TODO: Leer DB si existe
     }
+
+    get paramMapbox(){
+        return {
+            'access_token': "pk.eyJ1IjoiYWxkb25hdmFycmV0ZSIsImEiOiJja3Izc3pxamkxNGVtMnV0Y3Qxc2JicGFrIn0.6efzyciGBBR9rAxHwnao8g",
+            "limit": 5,
+            "language": "es"
+        }
+    }
     
     async buscarCiudad( lugar:string ):Promise<string[]>{
         //Peticion http
         try {
-            const resp = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/M%C3%A9xico.json?access_token=pk.eyJ1IjoiYWxkb25hdmFycmV0ZSIsImEiOiJja3Izc3pxamkxNGVtMnV0Y3Qxc2JicGFrIn0.6efzyciGBBR9rAxHwnao8g&limit=5&language=es");
-            console.log(resp);
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params:this.paramMapbox,
+                timeout: 1000,
+            });
+
+            const resp = await instance.get();
+
+            console.log(resp.data);
             return []; //Retornar los lugares que coincidan    
         } catch (error) {
             // throw error para reventar la aplicacion
