9f4eef4c7adb9c48a7ff4f38ffe141f36584fd62
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-15 12:55:54 -0500

    Tomando un template y acomodandolo en public

--- 06-Webserver/index.ts
+++ 06-Webserver/index.ts
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 
+//Servir contenido estatico con el path
+app.use( express.static('public')); //si hago esto lo de abajo jamas se va a ejecutar
+
+//Lo de abajo
 app.get('/',(req:any, res:any)=> {;
     res.send('Home page');
 })
+
+//
 app.get('/hola-mundo',(req:any, res:any)=> {
     res.send('Hello World/ Hello World');
 })
