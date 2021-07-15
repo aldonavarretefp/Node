7ed133a7af51a31ce5dcd9d069f9b86d977a1d3b
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-15 17:11:50 -0500

    Controladores para las rutas configurando router de Express

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors')
 
 class Server {
     //Usualmente las propiedades se declaran en constructor
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
-
+        this.usuariosPath = '/api/usuarios';
         //Middlewares   
         this.middlewares();
         //Rutas de mi aplicacion
@@ -15,41 +15,14 @@ class Server {
     }
 
     middlewares(){
+        //CORS
+        this.app.use(cors());
         //Directorio publico
         this.app.use(express.static('public'));
-        this.app.use(cors());
     }
     routes(){
-        //Endpoints:
-        //Obtener info
-        this.app.get('/api', (req, res)=> {
-            res.json({
-                msg: "get API"
-            });
-        })
-        //Actualizando data
-        this.app.put('/api', (req, res)=> {
-            res.json({
-                msg: "put API"
-            });
-        })
-        //Crear nuevos recursos
-        this.app.post('/api', (req, res)=> {
-            res.json({
-                msg: "post API"
-            });
-        })
-        //Borrar, marcandolo nadamas
-        this.app.delete('/api', (req, res)=> {
-            res.json({
-                msg: "delete API"
-            });
-        })
-        this.app.patch('/api', (req, res)=> {
-            res.json({
-                msg: "patch API"
-            });
-        })
+        //Configurando las rutas
+        this.app.use(this.usuariosPath,require("../routes/usuarios"));
     }
     listen(){
         this.app.listen(this.port,()=>{
