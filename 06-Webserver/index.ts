90c9dfe07c01f379893b21fec346e1d33aa901a5
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-15 13:48:39 -0500

    Renderizando pagina desde el backend

--- 06-Webserver/index.ts
+++ 06-Webserver/index.ts
@@ -4,19 +4,25 @@ const app = express()
 //Servir contenido estatico con el path
 app.use( express.static('public')); //si hago esto lo de abajo jamas se va a ejecutar
 
-//Lo de abajo
-app.get('/',(req:any, res:any)=> {;
-    res.send('Home page');
+app.set('view engine','hbs');
+
+app.get('/',(req:any, res:any)=> {
+
+    res.render("home",{
+        nombre:"Aldo",
+        titulo:"Curso de Node"
+    });
 })
 
-//
-app.get('/hola-mundo',(req:any, res:any)=> {
-    res.send('Hello World/ Hello World');
+app.get('/generic',(req:any, res:any)=> {
+    // res.send('Hello World/ Hello World');
+    res.sendFile(__dirname + "/public/generic.html");
 })
-app.get('/hola-mundo',(req:any, res:any)=> {
-    res.send('Hello World/ Hello World');
+app.get('/elements',(req:any, res:any)=> {
+    // res.send('Hello World/ Hello World');
+    res.sendFile(__dirname + "/public/elements.html")
 })
 app.get('*',(req:any, res:any) =>{
-    res.send('404 | PAGE NOT FOUND');
+    res.sendFile(__dirname+"/public/back/template/404.html")
 })
 app.listen(8080);
