38ec3466354d936d5729c2bd40c8052cba65b185
Aldo Navarrete <aaldiitoo@gmail.com>
2021-08-17 15:24:17 -0500

    Recibiendo un mensaje desde el frontend en tiempo real

--- 09-websocket-server/public/js/socket-client.js
+++ 09-websocket-server/public/js/socket-client.js
@@ -2,18 +2,36 @@ const socket = io();
 
 
 //Referencias del HTML
-const lblOnline = document.querySelector('#lblOnline');
+const lblOnline  = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar  = document.querySelector('#btnEnviar');
 
 
 socket.on('connect',() => {
-    console.log('Conectado');
-    lblOffline.style.display= "none";
+
+    console.log('Conectado al servidor');
     lblOnline.style.display= "";
+    lblOffline.style.display= "none";
     
 });
 socket.on('disconnect',() => {
+
     console.log('Desconectado del servidor');
-    lblOnline.style.display= "none";
     lblOffline.style.display= "";
+    lblOnline.style.display= "none";
+
+});
+
+btnEnviar.addEventListener('click',() => {
+
+    const mensaje = txtMensaje.value;
+
+    const payload = {
+        id:'123ABC',
+        msg:mensaje,
+        fecha:new Date().getTime()
+    }
+
+    socket.emit('enviar-mensaje',payload)
 });
