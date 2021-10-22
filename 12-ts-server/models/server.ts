4ffb10017011f608493ef16260da8376b5787993
aldonavarretefp <aaldiitoo@gmail.com>
2021-10-22 16:46:07 -0500

    middlewares y routes

--- 12-ts-server/models/server.ts
+++ 12-ts-server/models/server.ts
@@ -6,11 +6,31 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT || "8000";
+        this.middlewares();
+        this.routes();
     }
+
+    middlewares(){
+        //Cors
+        this.app.use(cors({
+
+        }));
+        // Body lecture
+        this.app.use(express.json());
+        
+        //Static folder
+        this.app.use(express.static('public'));
+
+    }
+
     listen(){
         this.app.listen(this.port,()=>{
             console.log(`Servidor corriendo en el puerto ${this.port}`);
         });
+    }
+
+    routes(){
+        this.app.use(this.paths.usuarios,userRoutes);
     }
 }
 export default Server;
